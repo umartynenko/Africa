673d94a3619180053ebbc4ca2b358a944620acb8
Martynenko Uriy <99603403+umartynenko@users.noreply.github.com>
2024-10-13 22:20:49 +0300

    Initial Commit

---
+++ AfricaTests/AfricaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaTests.swift
+//  AfricaTests
+//
+//  Created by Uriy Martynenko on 13.10.2024.
+//
+
+import Testing
+@testable import Africa
+
+struct AfricaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ AfricaUITests/AfricaUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AfricaUITestsLaunchTests.swift
+//  AfricaUITests
+//
+//  Created by Uriy Martynenko on 13.10.2024.
+//
+
+import XCTest
+
+final class AfricaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
