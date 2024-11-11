22f86c70c6bf3fae8c511c5d5fd4320634edc5d4
Martynenko Uriy <99603403+umartynenko@users.noreply.github.com>
2024-11-11 22:38:06 +0300

    013

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -7,38 +7,66 @@
 
 import SwiftUI
 
+
 struct GalleryView: View {
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    // SIMPLE GRID DEFINITION
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    // EFFICIENT GRID DEFINITION
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // DYNAMIC GRID LAYOUT
+    
+    func gridSwitch() {
+        withAnimation(.easeIn) {
+            gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn ))
+        }
+    }
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(.circle)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                // MARK: - Slider
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { oldValue, newValue in
+                        gridSwitch()
+                    }
+                
+                // MARK: - Grid
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.circle)
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: ForEach
+                }//: LazyVGrid
+                .onAppear {
+                    gridSwitch()
+                }
+            } //: VStack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: ScrollView
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationViewModel())
--- Africa/ViewModels/CoverImageViewModel.swift
+++ Africa/ViewModels/CoverImageViewModel.swift
@@ -9,19 +9,28 @@ import SwiftUI
 
 
 struct CoverImageViewModel: View {
+    @State private var currentIndex: Int = 0
+    
     // MARK: - PROPERTIES
     let coverImages: [CoverImageModel] = Bundle.main.decode("covers.json")
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     
     // MARK: - BODY
     var body: some View {
-        TabView {
-            ForEach(coverImages) { item in
-                Image(item.name)
+        TabView(selection: $currentIndex) {
+            ForEach(coverImages.indices, id: \.self) { index in
+                Image(coverImages[index].name)
                     .resizable()
                     .scaledToFill()
+                    .tag(index)
             } //: LOOP
         } //: TABVIEW
         .tabViewStyle(PageTabViewStyle())
+        .onReceive(timer) { _ in
+            withAnimation {
+                currentIndex = (currentIndex + 1) % coverImages.count
+            }
+        }
     }
 }
 
